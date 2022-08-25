765d2140a75070ef978c14620cd7c1f25a31a128
kushagra gupta <kushagraguptastud.02@gmail.com>
2022-08-25 23:50:18 +0530

    fun commit

---
+++ strings/OOPS.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class manu{
+
+};
+// class is user defined datatype 
+// agar khaali class bna do to fir object ko memory milegi 1 byte wrna utni milegi jitni properties ko milti hai 
+
+
+manu h1;
+
+
+class Hero{
+    private:
+    int health;
+
+    public:
+
+    char level;
+
+    int gethealth(){
+        return health;
+    }
+
+    void sethealth(int h){
+        health=h;
+
+    }
+  };
+
+    // sirf class ke andar hi private kre hue members ko use kra jaa skta hai class ke bahar inhe use nhi kra jaa skta
+    // agar use krna hai to getter aur setter ki madad se kro
+
+    int  main(){
+        Hero ramesh;
+        // ye hota hai data to statically allocate krna;
+        
+        cout<<ramesh.level<<endl;
+        cout<<ramesh.gethealth()<<endl;
+        ramesh.sethealth(70);
+        cout<<ramesh.gethealth()<<endl;
+    }
+
