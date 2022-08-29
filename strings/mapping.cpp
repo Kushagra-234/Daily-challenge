8563d580dff43926bc137418a549b23dde72506f
kushagra gupta <kushagraguptastud.02@gmail.com>
2022-08-29 21:30:48 +0530

    rainy day

---
+++ strings/mapping.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    string s="manua";
+    unordered_map<char,int>m;
+    for(int i=0;i<s.length();i++){
+        m[s[i]];
+    }
+
+    for(auto it:m){
+        cout<<it.first<<endl;
+    }
+
+   
+   
+return 0;
+}
